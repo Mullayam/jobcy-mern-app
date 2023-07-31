ca3c5d362b2d4b5c53bdf15d901a69cc22ff51c4
Mulayam S <mullayam06@gmail.com>
2023-07-31 22:35:52 +0530

    chnages in env file

--- src/connection/conn.ts
+++ src/connection/conn.ts
@@ -1,9 +1,9 @@
 import { PreSqlClient } from '@enjoys/presql'
 export const presql = new PreSqlClient({
-  pre_user: "root", //db username
-  pre_password: "", // dbpassword
-  pre_host: "localhost", //db host
-  pre_database: "jobcy", //database name
+  pre_user: process.env.DB_USER as string, //db username
+  pre_password: process.env.DB_PASS as string, // dbpassword
+  pre_host: process.env.DB_HOST as string || "localhost", //db host
+  pre_database: process.env.DB_NAME as string, //database name
   pre_port: 3306,
   showConnErrors: true, //optional field show connection log ,default false
   resultLogs: true, //optional field ,show results in console,default false
